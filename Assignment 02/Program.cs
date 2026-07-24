fa5c8a3ec1e17580ae433b575b9fbab854191d0f
Suliman <sulimanrageb015@gmail.com>
2026-07-24 19:44:30 +0300

    Q02

--- Assignment 02/Program.cs
+++ Assignment 02/Program.cs
@@ -8,3 +8,12 @@
 //Console.WriteLine(title);
 //Console.WriteLine(upperTitle);
 #endregion
+
+#region Q02
+//Requirements
+//Declare two separate string variables, both set to the literal "Clean Code". Use ReferenceEquals()
+//to check if they point to the same object in memory.
+//string title01 = "Clean Code";
+//string title02 = "Clean Code";
+//Console.WriteLine(object.ReferenceEquals(title01,title02));
+#endregion
