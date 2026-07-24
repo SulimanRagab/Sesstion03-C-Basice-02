dbce83f837d01a54cc6a8079e095ee6ca2f6d922
Suliman <sulimanrageb015@gmail.com>
2026-07-24 19:37:47 +0300

    Q01

--- Assignment 02/Program.cs
+++ Assignment 02/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//introoo Questions
+#region Q01
+//Requirements
+//Declare string title = "clean code";. Call title.ToUpper() and store it in a new variable upperTitle. Print
+//both title and upperTitle to show that title did not change.
+//string title = "clean code";
+//string upperTitle = title.ToUpper();
+//Console.WriteLine(title);
+//Console.WriteLine(upperTitle);
+#endregion
