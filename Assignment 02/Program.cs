becbab468818b886c2623f4129c86584a7596530
Suliman <sulimanrageb015@gmail.com>
2026-07-24 23:59:45 +0300

    Q14

--- Assignment 02/Program.cs
+++ Assignment 02/Program.cs
@@ -134,3 +134,14 @@
 //    i++;
 //}
 #endregion
+
+#region Q14
+//Write a do-while loop that prints "Checking book..." exactly 3 times
+//int i = 0;
+//do
+//{
+//    Console.WriteLine("Checking book...");
+//    i++;
+//} while (i < 3);
+#endregion
+
