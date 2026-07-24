654af2739408ea41288dd0f406585fa18c3c282a
Suliman <sulimanrageb015@gmail.com>
2026-07-24 23:55:08 +0300

    Q13

--- Assignment 02/Program.cs
+++ Assignment 02/Program.cs
@@ -124,3 +124,13 @@
 //    Console.WriteLine($"{i+1}.{books[i]}");
 //}
 #endregion
+
+#region Q13
+//Using the same books array, use a while loop to print every book title.
+//int i = 0;
+//while(i < books.Length)
+//{
+//    Console.WriteLine(books[i]);
+//    i++;
+//}
+#endregion
